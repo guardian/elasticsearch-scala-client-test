3a0435b1201cb7e71df47b004ff374310e63d6f8
Amy Hughes <amy.hughes@theguardian.com>
2018-01-18 11:43:53 +0000

    initial commit

---
+++ build.sbt
@@ -0,0 +1,3 @@
+scalaVersion := "2.11.11"
+
+libraryDependencies += "org.elasticsearch" % "elasticsearch" % "2.4.1"
---
+++ src/main/scala/com/gu/DemoFilterClient.scala
@@ -0,0 +1,8 @@
+package com.gu
+
+import org.elasticsearch.action._
+import org.elasticsearch.client.{Client, FilterClient}
+
+class DemoFilterClient(underlyingClient: Client) extends FilterClient(underlyingClient) {
+  override def doExecute[Request <: ActionRequest[_], Response <: ActionResponse, RequestBuilder <: ActionRequestBuilder[Request, Response, RequestBuilder]](action: Action[Request, Response, RequestBuilder], request: Request, listener: ActionListener[Response]) = super.doExecute(action, request, listener)
+}
